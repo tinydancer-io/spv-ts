bbf7261d80d79f263c305a150822daf2e8b54d88
anoushk1234 <kharangateanoushk04@gmail.com>
2024-06-05 13:15:28 +0530

    fixes unidici

--- test.ts
+++ test.ts
@@ -1,6 +1,12 @@
 import { AccountInfo, PublicKey } from "@solana/web3.js";
 import { monitorAndVerifyUpdates, DEFAULT_RPC_URL, DEFAULT_PK } from "./client";
 import { getCopyAccount, getCopyProgram } from "./program";
+import { setGlobalDispatcher, Agent } from "undici";
+setGlobalDispatcher(
+  new Agent({
+    connections: 100,
+  })
+);
 
 async function main() {
   let program  = getCopyProgram(DEFAULT_RPC_URL,new Uint8Array(DEFAULT_PK));
