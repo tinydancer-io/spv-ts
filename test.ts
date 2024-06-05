606e02af82633bcd1fbeadfdd8552abc4f3232ca
anoushk1234 <kharangateanoushk04@gmail.com>
2024-06-05 13:12:12 +0530

    fixes

--- test.ts
+++ test.ts
@@ -5,7 +5,7 @@ import { getCopyAccount, getCopyProgram } from "./program";
 async function main() {
   let program  = getCopyProgram(DEFAULT_RPC_URL,new Uint8Array(DEFAULT_PK));
   let account_pubkey = new PublicKey("HPUJAf6r3zJrkM72wB3EhGGtfbTkQwMPMSq6d7HaapYr");
-  let account_state = await program.account.copyAccount.getAccountInfo(account_pubkey);
+  let account_state = await program.provider.connection.getAccountInfo(account_pubkey);
   await monitorAndVerifyUpdates(account_pubkey,account_state as AccountInfo<Buffer>);
 }
 main();
